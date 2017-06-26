cefd277e11c5074da4622e5d3abca2a92ac56b70
Mitya <dmersiyanov@yandex.ru>
2017-06-26 07:58:59 +0300

    add list view design

---
+++ app/src/main/java/com/example/mitya/loftmoneytracker/Item.java
@@ -0,0 +1,17 @@
+package com.example.mitya.loftmoneytracker;
+
+/**
+ * Created by Mitya on 23.06.2017.
+ */
+
+public class Item {
+    final String name;
+    final int price;
+
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+
+}
