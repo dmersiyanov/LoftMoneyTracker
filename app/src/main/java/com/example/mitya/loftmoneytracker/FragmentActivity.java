8a4303463d7a862c978b3222b79c12167b360df9
Dmitriy Mersianov <dmersianov@Dmitriys-MacBook-Pro.local>
2017-06-27 16:00:51 +0300

    try to add balance fragment

--- app/src/main/java/com/example/mitya/loftmoneytracker/FragmentActivity.java
+++ app/src/main/java/com/example/mitya/loftmoneytracker/FragmentActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
 
 
 public class FragmentActivity extends AppCompatActivity {
@@ -16,8 +17,11 @@ public class FragmentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fragments);
         final TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
         final ViewPager pages = (ViewPager) findViewById(R.id.pages);
+        final LinearLayout layout = (LinearLayout) findViewById(R.id.balance);
+
         pages.setAdapter(new MainPagerAdapter());
         tabs.setupWithViewPager(pages);
+
     }
 
     private class MainPagerAdapter extends FragmentPagerAdapter {
@@ -32,19 +36,19 @@ public class FragmentActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-//            if (position == getCount() - 1)
-//                return new BalanceFragment();
-//            Bundle args = new Bundle();
-//            args.putString("type", types[position]);
-//
-//            final ItemsFragment itemsFragment = new ItemsFragment();
-//            itemsFragment.setArguments(args);
-//            return itemsFragment;
-            final ItemsFragment fragment = new ItemsFragment();
+            if (position == getCount() - 1)
+                return new BalanceFragment();
             Bundle args = new Bundle();
-            args.putString(ItemsFragment.ARG_TYPE, Item.TYPE_EXPENSE);
-            fragment.setArguments(args);
-            return fragment;
+            args.putString("type", types[position]);
+
+            final ItemsFragment itemsFragment = new ItemsFragment();
+            itemsFragment.setArguments(args);
+            return itemsFragment;
+//            final ItemsFragment fragment = new ItemsFragment();
+//            Bundle args = new Bundle();
+//            args.putString(ItemsFragment.ARG_TYPE, Item.TYPE_EXPENSE);
+//            fragment.setArguments(args);
+//            return fragment;
 
         }
 
