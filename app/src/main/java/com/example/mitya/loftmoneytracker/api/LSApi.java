07fdee4f6e94de53b551f6b24984ba8b537eadc8
Dmitriy Mersianov <dmersianov@Dmitriys-MacBook-Pro.local>
2017-06-29 15:34:54 +0300

    exercise #5 add private void addItem() method

--- app/src/main/java/com/example/mitya/loftmoneytracker/api/LSApi.java
+++ app/src/main/java/com/example/mitya/loftmoneytracker/api/LSApi.java
@@ -1,6 +1,12 @@
 package com.example.mitya.loftmoneytracker.api;
 
+import com.example.mitya.loftmoneytracker.Item;
+
+import java.util.List;
+
 import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
@@ -9,9 +15,16 @@ import retrofit2.http.Query;
  */
 
 public interface LSApi {
+
+    @Headers("Content-Type: application/json")
+    @GET("items")
+    Call<List<Item>> items(@Query("type") String type);
+
+
     @POST("items/add")
     Call<AddResult> add(@Query("name") String name,
                         @Query("price") int price, @Query("type") String type);
+
 
     @POST("items/remove")
     Call<Result> remove(@Query("id") int id);
