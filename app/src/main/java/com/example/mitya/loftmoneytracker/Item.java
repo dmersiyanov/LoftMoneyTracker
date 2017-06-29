d79b790ac80c0e790feb9dcadf9b80dcb5adab8e
Mitya <dmersiyanov@yandex.ru>
2017-06-29 09:17:37 +0300

    add api package working on exercise #5 neew to find how to add interceptor

--- app/src/main/java/com/example/mitya/loftmoneytracker/Item.java
+++ app/src/main/java/com/example/mitya/loftmoneytracker/Item.java
@@ -1,16 +1,21 @@
 package com.example.mitya.loftmoneytracker;
 
+import java.io.Serializable;
+
 /**
  * Created by Mitya on 23.06.2017.
  */
 
-public class Item {
-    final String name;
+public class Item implements Serializable {
+    public String name;
+    public String type;
     final int price;
+    public int id = -1;
 
-    public Item(String name, int price) {
+    public Item(String name, int price, String type) {
         this.name = name;
         this.price = price;
+        this.type = type;
     }
 
     static final String TYPE_EXPENSE = "expense";
