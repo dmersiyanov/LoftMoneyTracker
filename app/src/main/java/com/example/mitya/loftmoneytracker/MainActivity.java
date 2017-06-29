66f1d5a235ee45482245b3edf854c4e2a4638ab3
Mitya <dmersiyanov@yandex.ru>
2017-06-29 07:37:58 +0300

    hopefully the last fixes for exercise #4

--- app/src/main/java/com/example/mitya/loftmoneytracker/MainActivity.java
+++ app/src/main/java/com/example/mitya/loftmoneytracker/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.mitya.loftmoneytracker;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,20 +13,46 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.items);
-        final RecyclerView items = (RecyclerView) findViewById(R.id.items);
-        items.setAdapter(new ItemsAdapter());
+        setContentView(R.layout.activity_fragments);
+        final TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        final ViewPager pages = (ViewPager) findViewById(R.id.pages);
+
+        pages.setAdapter(new MainPagerAdapter());
+        tabs.setupWithViewPager(pages);
+        getSupportActionBar().setElevation(0);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    private class MainPagerAdapter extends FragmentPagerAdapter {
+        private final String[] titles;
+        private final String[] types = {Item.TYPE_EXPENSE, Item.TYPE_INCOME};
+
+        MainPagerAdapter() {
+            super(getSupportFragmentManager());
+            titles = getResources().getStringArray(R.array.main_pager_titles);
+        }
+
+
+        @Override
+        public Fragment getItem(int position) {
+            Bundle args = new Bundle();
+            if (position == getCount() - 1)
+                return new BalanceFragment();
+            args.putString("type", types[1]);
+            args.putString("type", types[position]);
+
+            final ItemsFragment itemsFragment = new ItemsFragment();
+            itemsFragment.setArguments(args);
+            return itemsFragment;
+        }
+
+        @Override
+        public int getCount() {
+            return titles.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-
 }
